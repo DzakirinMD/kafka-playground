776dac3145a14f89d795e19a67dfb850fdcfaf62
Dzakirin <m.abdrahim@dxc.com>
2022-08-03 16:03:12 +0800

    added REST API to send message to topic

---
+++ springboot-kafka-playground/src/main/java/com/dzakirinmd/springbootkafkaplayground/controller/MessageController.java
@@ -0,0 +1,29 @@
+package com.dzakirinmd.springbootkafkaplayground.controller;
+
+import com.dzakirinmd.springbootkafkaplayground.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+//    public MessageController(KafkaProducer kafkaProducer) {
+//        this.kafkaProducer = kafkaProducer;
+//    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message) {
+
+        kafkaProducer.sendMessage(message);;
+
+        return ResponseEntity.ok("Message has been sent to the topic!");
+    }
+}
